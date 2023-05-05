7a5cc398f4a22221a91eab93df4db7f8e11d8db8
daemon369 <daemon336699@gmail.com>
2023-05-05 17:58:50 +0800

    add more functions

--- src/Daemon.jl
+++ src/Daemon.jl
@@ -3,7 +3,7 @@ module Daemon
 export timestamp
 export v, d, i, w, e, p, setShowPrefix, logLevel, setLogLevel, getLogLevel, VERBOSE, DEBUG, INFO, WARNING, ERROR, QUIET
 export unzip
-export checkAdb, selectSerial
+export checkAdb, selectSerial, getProp, showProp, setProp, resetProp, resetProps
 
 include("macro.jl")
 include("common.jl")
--- src/android.jl
+++ src/android.jl
@@ -3,14 +3,28 @@ struct AndroidDevice
     state::String
 end
 
-function parseAndroidDevice(deviceInfo::AbstractString)
+noDevice = AndroidDevice("", "")
+
+
+"""
+    parseAndroidDevice(deviceInfo::AbstractString)::AndroidDevice
+
+Parse android device info line to AndroidDevice
+"""
+function parseAndroidDevice(deviceInfo::AbstractString)::AndroidDevice
     if isempty(deviceInfo)
-        return Nothing
+        return noDevice
     end
     local s = split(deviceInfo)
     return AndroidDevice(s[1], s[2])
 end
 
+
+"""
+    checkAdb()
+
+Check whether adb command is accessable
+"""
 function checkAdb()
     local adb = Sys.which("adb")
     if isempty(adb)
@@ -18,12 +32,16 @@ function checkAdb()
     end
 end
 
-#=
-selectSerial
-If multi-devices exists, let user choose one and return it's serial;
-if only one device exists, choose that one and return it's serial;
-if no device exists, throw ErrorException
-=#
+
+"""
+    selectSerial()::String
+
+Select android device and return it's serial
+
+1. If multi-devices exists, let user choose one and return it's serial;
+2. If only one device exists, choose that one and return it's serial;
+3. If no device exists, throw ErrorException
+"""
 function selectSerial()::String
     checkAdb()
     local adbDevices::String = readchomp(`adb devices`)
@@ -35,7 +53,7 @@ function selectSerial()::String
     for i in 2:lastindex(lines)
         if !isempty(lines[i])
             local d = parseAndroidDevice(lines[i])
-            if d isa AndroidDevice
+            if d != noDevice
                 push!(devices, d)
             end
         end
@@ -65,4 +83,121 @@ function selectSerial()::String
             end
         end
     end
+end
+
+
+"""
+    getProp(propKey)::AbstractString
+
+Get android prop by propKey
+"""
+function getProp(propKey)::AbstractString
+    return readchomp(`adb shell getprop $propKey`)
+end
+
+
+"""
+    showProp(props)
+
+Print android props
+"""
+function showProp(props)
+    p("==============================================================")
+    p(">> get prop\n")
+
+    d("typeof(props): $(typeof(props))")
+
+    if props isa String
+        v("is a String")
+        local splited = split(props)
+        if length(splited) == 1
+            prop = getProp(props)
+            println("$props=$prop")
+        else
+            showProp(splited)
+        end
+    elseif isa(props, Vector) || isa(props, Array) || isa(props, Dic)
+        if isa(props, Vector)
+            v("is a Vector")
+        elseif isa(props, Array)
+            v("is a Array")
+        elseif isa(props, Dic)
+            v("is a Dic")
+        end
+        for x in props
+            prop = getProp(x)
+            println("$x=$prop")
+        end
+    end
+
+    p("\n<< get prop")
+    p("==============================================================")
+end
+
+
+"""
+    setProp(prop::AbstractString, value::AbstractString)
+
+Set android prop to new value
+"""
+function setProp(prop::AbstractString, value::AbstractString)
+    p("==============================================================")
+    p(">> set prop $(prop) $(value)\n")
+
+    before = getProp(prop)
+
+    run(`adb shell setprop $prop $value`)
+
+    after = getProp(prop)
+
+    p("$before=>$(after)")
+
+    p("\n<<  set prop $(prop) $(value)")
+    p("==============================================================")
+end
+
+
+"""
+    resetProp(prop)
+
+Reset android prop to empty
+"""
+function resetProp(prop)
+    d("resetProp($(prop))")
+    local current = getProp(prop)
+    p(prop)
+    d("resetProp current=$current")
+    readchomp(`adb shell setprop $prop \"\"`)
+end
+
+
+"""
+    resetProps(props)
+
+Reset android props to empty
+"""
+function resetProps(props)
+    p("==============================================================")
+    p(">> reset props\n")
+
+    d("typeof(props): $(typeof(props))")
+
+    if props isa String
+        v("is a String")
+        resetProp(props)
+    elseif isa(props, Vector) || isa(props, Array) || isa(props, Dic)
+        if isa(props, Vector)
+            v("is a Vector")
+        elseif isa(props, Array)
+            v("is a Array")
+        elseif isa(props, Dic)
+            v("is a Dic")
+        end
+        for x in props
+            resetProp(x)
+        end
+    end
+
+    p("\n<< reset props")
+    p("==============================================================")
 end
